638c93464b8744390e9126e53a185e3ecf190774
Monowar Hussain <monowargtp@gmail.com>
2023-05-25 15:58:59 +0530

    add only website files on this branch

--- test/test.js
+++
@@ -1,7 +0,0 @@
-const test = require('maxvalid')
-
-//test your any functions here
-
-
-const testcase = test.validateEmail('monowargtp@gmail.com');
-console.log("This function pass the testcase :"+" "+(testcase)); 
