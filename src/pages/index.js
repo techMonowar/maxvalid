834a9ff195f42c4be0110345a40bb66ab8962a50
Monowar Hussain <monowargtp@gmail.com>
2023-05-26 05:51:48 +0530

    SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Codehighlight from '../../components/codeHighlighter'
@@ -87,6 +88,11 @@ export default function Home() {
 
 
   return (
+    <>
+     <Head> 
+      <title>Maxvalid - a package for maximum validation</title>
+      </ Head>
+
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-400 dark:bg-zinc-400/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-300/30">
@@ -223,5 +229,6 @@ export default function Home() {
        <Codehighlight  language='javascript' code={angularjs} /></div>
       </div>
     </main>
+    </>
   )
 }
