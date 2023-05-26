44e7ab2051c1a97e6d83989b39e0efdfe5c9eb6d
Monowar Hussain <monowargtp@gmail.com>
2023-05-26 09:01:45 +0530

    minor

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
 
    
 
-    <main className="flex w-screen min-h-screen flex-col items-center justify-between lg:p-24 pt-20 p-4">
+    <main className="flex  min-h-screen flex-col items-center justify-between lg:p-24 pt-20 p-8">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0  flex  w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-400 dark:bg-zinc-400/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-300/30">
           Get started by&nbsp;
